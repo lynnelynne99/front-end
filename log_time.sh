373b472683dfcd4563afaccb26c10db6b59acaed
lynnelynne99 <lynnelynne99@naver.com>
2024-06-12 14:39:36 +0900

    현석

--- log_time.sh
+++ log_time.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-LOG_FILE="/home/ubuntu/workspace/test/frontEnd/aa.txt"
-CURRENT_TIME=$(date "+%Y-%m-%d %H:%M:%S")
-echo $CURRENT_TIME >> $LOG_FILE
+date >> aa.txt
 git add .
 git commit -m "현석"
 git push
